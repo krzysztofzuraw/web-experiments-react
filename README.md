ec8216dfecc68f576510da907f24176598808d4c
Krzysztof Żuraw <services@kzuraw.com>
2020-12-29 17:32:20 +0100

    feat: last fixes

---
+++ README.md
@@ -0,0 +1,5 @@
+# Web Experiments in React
+
+## Timeline
+
+Credits & idea: [Creating UI Components in SVG | CSS-Tricks](https://css-tricks.com/creating-ui-components-in-svg/)
--- pages/timeline.tsx
+++ pages/timeline.tsx
@@ -1,40 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from '../styles/timeline.module.css';
 
-const items = ['one', 'two', 'three', 'four'];
+const items = [
+  { text: 'one', clicked: false },
+  { text: 'two', clicked: false },
+  { text: 'three', clicked: false },
+  { text: 'four', clicked: false },
+];
+const margin = 33;
+const spacing = 15;
 
 export default function Timeline() {
+  const [timelineItems, setTimeLineItems] = useState(items);
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.sections}>
         <section>
           <svg
-            viewBox={`0 0 50 ${items.length * 30}`}
+            viewBox={`0 0 30 ${timelineItems.length * 30}`}
             xmlns="http://www.w3.org/2000/svg"
-            width="100px"
             stroke="currentColor"
             fill="white"
             aria-labelledby="timeline"
             role="presentation"
           >
             <title id="timeline">timeline element</title>
-            <line x1="10" x2="10" y1="150" y2="0" />
-            {items.map((item, idx) => (
+            <line x1="10" x2="10" y1={spacing} y2={timelineItems.length * margin - spacing} />
+            {timelineItems.map((item, idx) => (
               <circle
-                key={`${item}-${idx}`}
+                key={`${item.text}-${idx}`}
                 cx="10"
-                cy={`${10 + 25 * idx}`}
+                cy={`${idx * margin + spacing}`}
                 r="5"
-                style={{ fill: 'white', stroke: 'black' }}
+                style={{ fill: item.clicked ? 'black' : 'white', stroke: 'black' }}
               />
             ))}
           </svg>
         </section>
         <section>
           <ol className={styles.list}>
-            {items.map((item, idx) => (
-              <li key={`${idx}`}>{item}</li>
+            {timelineItems.map((item, idx) => (
+              <li
+                key={`${idx}`}
+                onClick={() => {
+                  const updatedItems = timelineItems.map((el) =>
+                    el.text === item.text ? { text: item.text, clicked: true } : el
+                  );
+                  setTimeLineItems(updatedItems);
+                }}
+              >
+                {item.text}
+              </li>
             ))}
           </ol>
         </section>
